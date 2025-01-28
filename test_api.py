9af1b8b3f675f11c52c20a48be0700942d1ecdc1
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-28 11:51:24 -0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
 
+
 from fastapi.testclient import TestClient
 from learning_api import app # Importing our main FastAPI app
-import pytest 
+import pytest
 
 #Create test client
 client = TestClient(app)
@@ -13,12 +14,12 @@ def test_add_learning_progress():
         "topic": "Python", # using our LearningTopic enum
         "hours_spent": 2.5, # valid hourse (between 0-24)
         "difficulty_level": 3, # valid level (1-5)
-        "notes": "Learning FastAPI testint", # more than 10 charts
+        "notes": "Learning FastAPI testing", # more than 10 charts
         "understanding_level": 8, # valid level (1-10)
         "questions": ["How do we handle test database?"]
     }
 
     response = client.post("/add-progress", json=test_data) 
     assert response.status_code == 200 # Check if request was successful
-    assert "Progress update successfully !" in response.json()["message"]
-      
+    assert "Progress updated successfully!" in response.json()["message"]
+
