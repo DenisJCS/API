9028c3bed5e288513e3b8313c5053d87f6fbf3ef
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-01-28 11:51:46 -0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -23,3 +23,20 @@ def test_add_learning_progress():
     assert response.status_code == 200 # Check if request was successful
     assert "Progress updated successfully!" in response.json()["message"]
 
+Intensive2 % python -m pytest test_api.py -v
+========================================== test session starts ==========================================
+platform darwin -- Python 3.12.3, pytest-8.3.4, pluggy-1.5.0 -- /Users/denis_jcs/Documents/Intensive2/venv/bin/python
+cachedir: .pytest_cache
+rootdir: /Users/denis_jcs/Documents/Intensive2
+plugins: anyio-4.8.0
+collected 1 item                                                                                        
+
+test_api.py::test_add_learning_progress PASSED                                                    [100%]
+
+=========================================== warnings summary ============================================
+test_api.py::test_add_learning_progress
+  /Users/denis_jcs/Documents/Intensive2/learning_api.py:128: PydanticDeprecatedSince20: The `dict` method is deprecated; use `model_dump` instead. Deprecated in Pydantic V2.0 to be removed in V3.0. See Pydantic V2 Migration Guide at https://errors.pydantic.dev/2.10/migration/
+    "data": update.dict()
+
+-- Docs: https://docs.pytest.org/en/stable/how-to/capture-warnings.html
+===================================== 1 passed, 1 warning in 0.22s ======================================
